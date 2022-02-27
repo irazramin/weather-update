19146e56979b580924bcb2f84c5b8116674e9d09
irazramin <irazramin@gmail.com>
2022-02-27 10:44:06 +0600

    todays UI part done

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 // https://api.openweathermap.org/data/2.5/forecast?q=tangail&appid=f74593b83563c031a2b9557c7705e26d
 // API KEY : f74593b83563c031a2b9557c7705e26d
+
+const searchBarOpening = document.getElementById('search-input');
+const searchCity = document.getElementById('search-city');
+searchBarOpening.addEventListener('click',() =>{
+    searchBarOpening.classList.add('opening');
+    searchCity.focus()
+})
