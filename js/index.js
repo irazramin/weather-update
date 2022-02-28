5f25adee63861df9cbf52e008e9b1a61996562b9
irazramin <irazramin@gmail.com>
2022-02-28 13:01:34 +0600

    weather update UI part done

--- js/index.js
+++ js/index.js
@@ -8,9 +8,15 @@ const weatherSituation = document.getElementById('weather-situation');
 const weatherIcon = document.getElementById('weather-icon');
 const dayTo = document.getElementById('day');
 const time = document.getElementById('hour');
+const uvIndex = document.getElementById('uv-index');
+const windStatus = document.getElementById('wind-status');
+const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
+const humidity = document.getElementById('humidity');
+const visibility = document.getElementById('visibility');
+const airQuality = document.getElementById('air-quality');
 const todayTemps = document.getElementById('today-temp');
 const fullWeekName = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'];
-const shortWeekName = ['Sun','Mon','Tue','Wed','Thurs','Fri'];
 searchBarOpening.addEventListener('click',() =>{
     searchBarOpening.classList.add('opening');
     searchCity.focus()
@@ -38,29 +44,36 @@ const getWeatherApi = async  (city) =>{
           `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`
         );
         const data  = await res.json();
-        // fetch data
-        const weather = data.list[0].weather[0];
-        // js date
-        const wD = `${data.list[0].dt_txt.slice(0, 10)}`;
-        const date = new Date(wD);
-        const date2 = new Date();
-        const day = date.getDay();
-        const temp = Math.round(data.list[0].main.temp);
-        // set data to elements
-        todayTemps.innerText = `${temp}°`;
-        time.innerText = `${date2.getHours()} : ${date2.getMinutes()}`
-        dayTo.innerText = `${fullWeekName[day]},`;
-        console.log(date);
-        weatherSituation.innerText = weather.description;
-        weatherIcon.src = `http://openweathermap.org/img/w/${weather.icon}.png`;
-        console.log(date.getHours());
-            loader.style.display = 'none';
-
+        todayWeatherData(data);
+        otherUpdates(data);
+        loader.style.display = 'none'
     } catch (error) {
         console.log(error);
-        
+        loader.style.display = 'none';
     }
 }
 
 getWeatherApi('dhaka');
 
+const todayWeatherData = (data) =>{
+  // fetch data
+  const weather = data.list[0].weather[0];
+  // js date
+  const wD = `${data.list[0].dt_txt.slice(0, 10)}`;
+  const date = new Date(wD);
+  const date2 = new Date();
+  const day = date.getDay();
+  const temp = Math.round(data.list[0].main.temp);
+  // set data to elements
+  todayTemps.innerText = `${temp}°`;
+  time.innerText = `${date2.getHours()} : ${date2.getMinutes()}`;
+  dayTo.innerText = `${fullWeekName[day]},`;
+  weatherSituation.innerText = weather.description;
+  weatherIcon.src = `http://openweathermap.org/img/w/${weather.icon}.png`;
+}
+
+const otherUpdates = (data) =>{
+    const weatherData = data.list[0];
+    windStatus.innerText = `${weatherData.wind.speed}`;
+    console.log(weatherData.wind.speed);
+}
