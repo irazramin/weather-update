39cb5887b42e947e9823d2a760963823ec86651e
irazramin <irazramin@gmail.com>
2022-03-05 07:29:02 +0600

    weekly update feature done and add little responsive

--- js/index.js
+++ js/index.js
@@ -16,7 +16,20 @@ const humidity = document.getElementById('humidity');
 const visibility = document.getElementById('visibility');
 const airQuality = document.getElementById('air-quality');
 const todayTemps = document.getElementById('today-temp');
-const fullWeekName = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'];
+const fullWeekName = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const halfWeekName = ['Sun','Mon','Tue','Wed','Thu','Fri'];
+const weeklyUpdateContainer = document.getElementById(
+'weekly-update-container');
+const today = document.getElementById('today');
+const menuBar = document.getElementById('menu-bar');
+
+
+// add sidebar 
+menuBar.addEventListener('click',() =>{
+  today.classList.toggle('sidebar');
+})
+
+
 searchBarOpening.addEventListener('click',() =>{
     searchBarOpening.classList.add('opening');
     searchCity.focus()
@@ -35,9 +48,26 @@ const getCityName = () =>{
 }
 getCityName();
 
+
+const getWeatherApiByLoc = async (lat,lon) => {
+        try {
+        // fetch
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+        );
+        const data  = await res.json();
+        todayWeatherData(data);
+        otherUpdates(data);
+        fiveDaysForecast(data);
+        loader.style.display = 'none'
+    } catch (error) {
+        console.log(error);
+        loader.style.display = 'none';
+    }
+}
+
 const getWeatherApi = async  (city) =>{
     searchCity.value = '';
-    console.log(city);
     try {
         // fetch
         const res = await fetch(
@@ -46,6 +76,7 @@ const getWeatherApi = async  (city) =>{
         const data  = await res.json();
         todayWeatherData(data);
         otherUpdates(data);
+        fiveDaysForecast(data);
         loader.style.display = 'none'
     } catch (error) {
         console.log(error);
@@ -53,7 +84,23 @@ const getWeatherApi = async  (city) =>{
     }
 }
 
-getWeatherApi('dhaka');
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+   console.log('something wrong');
+  }
+}
+
+function showPosition(position) {
+    getWeatherApiByLoc(position.coords.latitude,position.coords.longitude);
+    console.log(position.coords.latitude);
+}
+
+getLocation()
+
+
 
 const todayWeatherData = (data) =>{
   // fetch data
@@ -64,6 +111,7 @@ const todayWeatherData = (data) =>{
   const date2 = new Date();
   const day = date.getDay();
   const temp = Math.round(data.list[0].main.temp);
+
   // set data to elements
   todayTemps.innerText = `${temp}°`;
   time.innerText = `${date2.getHours()} : ${date2.getMinutes()}`;
@@ -75,5 +123,36 @@ const todayWeatherData = (data) =>{
 const otherUpdates = (data) =>{
     const weatherData = data.list[0];
     windStatus.innerText = `${weatherData.wind.speed}`;
-    console.log(weatherData.wind.speed);
+}
+
+
+const fiveDaysForecast = (data) =>{
+    weeklyUpdateContainer.textContent = ''
+    for (let i = 0; i < data.list.length; i+=8) {
+        const weeklyData = data.list[i];
+        const listDates =data.list[i].dt_txt.split(' ')[0]; 
+        const date = new Date(listDates);
+        console.log(weeklyData.main.temp_max);
+        
+        const day = date.toString().split(' ')[0]
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+             <div class='weekly-update'>
+               <h6>${day}</h6>
+               <img src='img/cloudy.png' alt='' />
+               <p>
+                 <span id='weekly-max-temp'>${Math.round(
+                   weeklyData.main.temp_max
+                 )}°</span>-
+                 <span id='weekly-min-temp'>${Math.round(
+                   weeklyData.main.temp_min
+                 )}°</span>
+               </p>
+             </div>
+        `;
+        
+        weeklyUpdateContainer.appendChild(div);
+    }
+
 }
